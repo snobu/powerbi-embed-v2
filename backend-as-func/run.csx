8de97a21eb58b1c96f2c58e1919f11d10d923670
snobu <foo@snobu.org>
2017-08-26 12:03:45 +0300

    First.

---
+++ backend-as-func/run.csx
@@ -0,0 +1,84 @@
+using System.Net;
+using System.Configuration;
+using Microsoft.PowerBI.Api.V2;
+using Microsoft.PowerBI.Api.V2.Models;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Rest;
+
+private static readonly string Username = ConfigurationManager.AppSettings["pbiUsername"];
+private static readonly string Password = ConfigurationManager.AppSettings["pbiPassword"];
+private static readonly string AuthorityUrl = ConfigurationManager.AppSettings["authorityUrl"];
+private static readonly string ResourceUrl = ConfigurationManager.AppSettings["resourceUrl"];
+private static readonly string ClientId = ConfigurationManager.AppSettings["clientId"];
+private static readonly string ApiUrl = ConfigurationManager.AppSettings["apiUrl"];
+private static readonly string GroupId = ConfigurationManager.AppSettings["groupId"];
+
+public static async Task<HttpResponseMessage> Run(HttpRequestMessage req, TraceWriter log)
+{
+    HttpResponseMessage response;
+
+    log.Info("C# HTTP trigger function processed a request.");
+
+    // Create a user password cradentials.
+    var credential = new UserPasswordCredential(Username, Password);
+
+    // Authenticate using created credentials
+    var authenticationContext = new AuthenticationContext(AuthorityUrl);
+    var authenticationResult = await authenticationContext.AcquireTokenAsync(ResourceUrl, ClientId, credential);
+    log.Info($"We have a Bearer token: {authenticationResult.AccessToken.Substring(0,9)}");  
+
+    if (authenticationResult == null)
+    {
+        log.Error("Authentication Failed, authenticationResult is null.");
+    }
+
+    var tokenCredentials = new TokenCredentials(authenticationResult.AccessToken, "Bearer");
+
+    // Create a Power BI Client object. It will be used to call Power BI APIs.
+    using (var client = new PowerBIClient(new Uri(ApiUrl), tokenCredentials))
+    {
+        // Get a list of reports.
+        var reports = await client.Reports.GetReportsInGroupAsync(GroupId);
+
+        // Pick the 3rd report in group (also known as workspace)
+        var report = reports.Value.Skip(2).First();
+
+        if (report == null)
+        {
+            log.Error("Group has no reports.");
+        }
+
+        // Generate Embed Token.
+        var generateTokenRequestParameters = new GenerateTokenRequest(accessLevel: "view");
+        var tokenResponse = await client.Reports.GenerateTokenInGroupAsync(GroupId, report.Id, generateTokenRequestParameters);
+
+        if (tokenResponse == null)
+        {
+            log.Error("Failed to generate embed token.");
+        }
+
+        var tok = tokenResponse.Token;
+
+        // Generate Embed Configuration.
+        var embedConfig = new EmbedConfig()
+        {
+            embedToken = tokenResponse.Token,
+            embed_url = report.EmbedUrl,
+            report_id = report.Id
+        };
+
+        response = (embedConfig != null) ?
+            req.CreateResponse(HttpStatusCode.OK, embedConfig) :
+            req.CreateResponse(HttpStatusCode.InternalServerError,
+                "Things have gone horribly wrong.");
+    }
+
+    return response;
+}
+
+public class EmbedConfig
+{
+    public string embedToken { get; set; }
+    public string report_id { get; set; }
+    public string embed_url { get; set; }
+}
---
+++ frontend-as-webapp/js/app.js
@@ -0,0 +1,240 @@
+// Let's get an Azure AD Bearer token and
+// exchange it for PowerBI embed token.
+
+// Python backend
+// var backendUrl =  "/api/token";
+
+// OR Azure Function C# backend
+// Be careful how you're calling the Azure Function in PROD.
+// Always authenticate the user first in your frontend,
+// otherwise anyone with the Function-Level-Authorization-Token
+// can call your backend and get a valid embed token.
+// The better approach would be to use the built-in Azure AD
+// authentication feature in App Service (called EasyAuth)
+// and have authenticated calls to both the frontend Web App and
+// the Function App.
+var backendUrl = "https://FuncAppName.azurewebsites.net/api/FuncName?code=Function-Level-Authorization-Token";
+
+$.ajax({
+    url: backendUrl,
+    success: result => {
+        embed(result);
+    }
+});
+
+var report, reportDiv, accessToken, embedUrl, embedReportId;
+// Set filter defaults
+var outsourced = false;
+
+$(document).ready(() => {
+    if (backendUrl.includes('funcapps')) {
+        $('#appName').text('PowerBI Embedded Demo - C# Azure Function backend');
+    }
+    else {
+        $('#appName').text('PowerBI Embedded Demo - Python backend');
+    }
+});
+
+function embed(result) {
+    // Embed Token
+    accessToken = result.embedToken
+
+    // Read embed URL from Model
+    embedUrl = result.embed_url;
+
+    // Read report Id from Model
+    embedReportId = result.report_id;
+
+    // Get models. models contains enums that can be used.
+    var models = window['powerbi-client'].models;
+
+    // Embed configuration used to describe the what and how to embed.
+    // This object is used when calling powerbi.embed.
+    // This also includes settings and options such as filters.
+    // You can find more information at https://github.com/Microsoft/PowerBI-JavaScript/wiki/Embed-Configuration-Details.
+    var config = {
+        type: 'report',
+        tokenType: models.TokenType.Embed,
+        accessToken: accessToken,
+        embedUrl: embedUrl,
+        id: embedReportId,
+        permissions: models.Permissions.All,
+        settings: {
+                filterPaneEnabled: false,
+                navContentPaneEnabled: false
+        }
+    };
+
+    // Get a reference to the embedded report HTML element
+    reportDiv = $('#reportDiv')[0];
+
+    // Embed the report and display it within the div container
+    report = powerbi.embed(reportDiv, config);
+
+    // Wait for report to be loaded
+    // https://github.com/Microsoft/PowerBI-JavaScript/wiki/Handling-Events
+    report.on('loaded', event => {
+        // Set event handlers
+        $('#Mexico').click(() => {
+            report.setFilters([makeLocationFilter('Mexico')])
+                .then(result => {
+                    console.log('Location filter set to Mexico');
+                })
+                .catch(errors => {
+                    console.error(errors);
+                });
+        });
+        
+        // Be verbose about PowerBI events
+        report.off('dataSelected');
+        report.off('filtersApplied');
+        report.on('dataSelected', event => {
+            var data = event.detail;
+            console.log('[PowerBI dataSelected event]');
+            console.log(data);
+        });
+        report.on('filtersApplied', event => {
+            var data = event.detail;
+            console.log('[PowerBI filtersApplied event]');
+            console.log(data);
+        });
+
+        // UI Event handlers
+        $('#USA').click(() => {
+            report.setFilters([makeLocationFilter('USA')])
+                .then(result => {
+                    console.log('Location filter set to USA');
+                })
+                .catch(errors => {
+                    console.error(errors);
+                });
+        });
+
+        $('#Outsourced').click(() => {
+            if ($('#Outsourced').is(':checked')) {
+                report.setFilters([makeSubCatFilter('Outsourced')])
+                    .then(result => {
+                        console.log('Showing only Outsources subcategory.');
+                        outsourced = true;
+                    })
+                    .catch(errors => {
+                        console.error(errors);
+                    });
+                }
+            else if (outsourced) {
+                report.removeFilters([makeSubCatFilter('Outsourced')])
+                .then(result => {
+                    console.log('Outsourced subcategory filter removed.');
+                    outsourced = false;
+                })
+                .catch(errors => {
+                    console.error(errors);
+                });
+            }
+        });
+
+        $('#filterPane').click(function() {
+            if ($(this).is(':checked')) {
+                report.updateSettings({ 'filterPaneEnabled': true })
+                    .then(result => {
+                        console.log('Filter pane enabled.');
+                    })
+                    .catch(errors => {
+                        console.error(errors);
+                    });
+                }
+            else {
+                report.updateSettings({ 'filterPaneEnabled': false })
+                    .then(result => {
+                        console.log('Filter pane disabled.');
+                    })
+                    .catch(errors => {
+                        console.error(errors);
+                    });
+            }
+        });
+
+        $('#reportNav').click(function() {
+            if ($(this).is(':checked')) {
+                report.updateSettings({ 'navContentPaneEnabled': true })
+                    .then(result => {
+                        console.log('Navigation pane enabled.');
+                    })
+                    .catch(errors => {
+                        console.error(errors);
+                    });
+                }
+            else {
+                report.updateSettings({ 'navContentPaneEnabled': false })
+                    .then(result => {
+                        console.log('Navigation pane disabled.');
+                    })
+                    .catch(errors => {
+                        console.error(errors);
+                    });
+            }
+        });
+
+        $('#Print').click(function() {
+            report.print()
+            .then(function (result) {
+                console.log('Report is printing.');
+            })
+            .catch(function (errors) {
+                console.error(errors);
+            });
+        });
+
+        // Page Navigation buttons
+        report.getPages()
+            .then(pages => {
+                pages.forEach(page => {
+                    var pageNames = 'PageName: ' + page.name + ", DisplayName: " + page.displayName;
+                    var btnId = `pageBtn_${page.name}`;
+                    var btn = `<button class="btn btn-warning btn-block" style="font-size: 10px" id="${btnId}">${page.displayName}</button>`
+                    $('#pageButtons').append(btn);
+                    $('#pageButtons').show(150); // fade in
+                    var sel = $('#' + btnId);
+                    sel.click(() => {
+                        report.setPage(page.name)
+                            .then(result => {
+                                console.log(`Page set to ${page.name}`);
+                            })
+                            .catch(errors => {
+                                console.error(errors);
+                            });
+                    });
+                });
+            })
+            .catch(error => {
+                console.error(error);
+            });
+
+    }); // on 'loaded'
+} // embed func
+
+// Custom nav and filters
+function makeLocationFilter(location) {
+    return filter = {
+        $schema: "http://powerbi.com/product/schema#basic",
+        target: {
+            table: "Location",
+            column: "Country/Region"
+        },
+        operator: "Contains",
+        values: [location]
+    };
+}
+
+// Custom nav and filters
+function makeSubCatFilter(subcat) {
+    return filter = {
+        $schema: "http://powerbi.com/product/schema#basic",
+        target: {
+            table: "Item",
+            column: "Sub Category"
+        },
+        operator: "Contains",
+        values: [subcat]
+    };
+}
